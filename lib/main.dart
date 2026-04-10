59288703886a8de3b858b3684d15ddd45e573cf0
longnm2704 <minhlongitf@gmail.com>
2026-04-10 15:16:16 +0700

    feat: setup dependency injection with service locator and register core dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/widgets.dart';
 import 'package:starnyx/app/app.dart';
+import 'package:starnyx/app/di/service_locator.dart';
 import 'package:easy_localization/easy_localization.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await configureDependencies();
   await EasyLocalization.ensureInitialized();
 
   runApp(
