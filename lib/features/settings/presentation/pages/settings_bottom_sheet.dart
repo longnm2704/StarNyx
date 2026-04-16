669986a5a91cff60463174c47768385146feaace
longnm2704 <minhlongitf@gmail.com>
2026-04-16 15:25:06 +0700

    style(settings): remove all navigation transitions for internal sub-pages (STX-040)

--- lib/features/settings/presentation/pages/settings_bottom_sheet.dart
+++ lib/features/settings/presentation/pages/settings_bottom_sheet.dart
@@ -74,15 +74,7 @@ class _SettingsBottomSheetState extends State<SettingsBottomSheet> {
         }
       },
       transitionsBuilder: (context, animation, secondaryAnimation, child) {
-        const begin = Offset(1.0, 0.0);
-        const end = Offset.zero;
-        const curve = Curves.easeOutCubic;
-        var tween = Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
-
-        return SlideTransition(
-          position: animation.drive(tween),
-          child: child,
-        );
+        return child;
       },
     );
   }
